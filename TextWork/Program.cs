b5d6f16f8cb41e1b8f64575d7618042ea38cb3f0
Rui_dm <Rui_dm57@student.itstep.org>
2026-06-29 21:18:15 +0300

    //

--- TextWork/Program.cs
+++ TextWork/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine("Hello, World!");
  * Team:
  * Bereka Dmytro dmitriy.bereka@gmail.com
  * Kovalevskiy Dmytro kovalevskijdmitro29@gmail.com
- * ..
+ * Rui Dariia dasharuy2007@gmail.com
  * ..
  * ..
  */
