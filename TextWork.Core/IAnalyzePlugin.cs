1e07ff1e7d2f022b9bcd93520db005fa2f5e9c16
Vasyl Kovalov <raid3r81@gmail.com>
2026-06-29 20:56:41 +0300

    Add TextWork.Core project with plugin interface

---
+++ TextWork.Core/IAnalyzePlugin.cs
@@ -0,0 +1,12 @@
+﻿namespace TextWork.Core;
+
+public interface IAnalyzePlugin
+{
+    public string Name { get; }
+    public string Description { get; }
+    
+    public void Analyze(string text);
+    
+    public List<string> GetResults();
+    
+}
