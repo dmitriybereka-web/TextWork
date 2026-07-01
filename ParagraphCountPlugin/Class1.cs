21dcc2f52d9b3f3a5f2d5a6fcd456a63e3c225e3
GrinX_is77@student.itstep.org <GrinX_is77@student.itstep.org>
2026-07-01 19:34:03 +0300

    Added ParagraphCountPlugin

---
+++ ParagraphCountPlugin/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TextWork.Core;
+
+namespace ParagraphCountPlugin
+{
+    public class ParagraphCountPlugin : IAnalyzePlugin
+    {
+        public string Name => "Paragraph Counter";
+
+        public string Analyze(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return "Paragraphs: 0";
+
+            string[] paragraphs = text.Split(
+                new[] { "\r\n\r\n", "\n\n" },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            return $"Paragraphs: {paragraphs.Length}";
+        }
+    }
+}
